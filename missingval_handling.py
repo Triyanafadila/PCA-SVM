829dcbb2eaa7e6161ab8950d2f7384d197fdbc96
Triyanafadila <triyanafadila@gmail.com>
2020-01-27 12:38:43 +0700

    bismillah

--- missingval_handling.py
+++ missingval_handling.py
@@ -3,11 +3,10 @@ import pandas as pd
 from sklearn.preprocessing import Imputer
 
 df = pd.read_csv("D:\hepatitis\dataset_missing_num.csv")
-7
+
 imp = Imputer(strategy='most_frequent')
 imp.fit(df)
-df = imp.transform(df)
+df = imp.transform(77df)
 
-df = pd.DataFrame(df)
+d7f = pd.DataFrame(df)
 df.to_csv("D:\hepatitis\hepatitis_most.txt")
-7
