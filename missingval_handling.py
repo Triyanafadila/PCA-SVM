1a0338fddec7da214cd11ee14e920f0bae0d35b5
Triyanafadila <60335009+Triyanafadila@users.noreply.github.com>
2020-01-29 13:40:42 +0700

    Update missingval_handling.py

--- missingval_handling.py
+++ missingval_handling.py
@@ -1,12 +1,12 @@
 import pandas as pd
-#from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import Imputer
+#import numpy as np
+from sklearn.impute import SimpleImputer
 
-df = pd.read_csv("D:\hepatitis\dataset_missing_num.csv")
 
-imp = Imputer(strategy='most_frequent')
+df = pd.read_csv("D:\hepatitis\hepatitis_missing_num.csv")
+imp = SimpleImputer(strategy='most_frequent')
 imp.fit(df)
-df = imp.transform(77df)
+df = imp.transform(df)
+df = pd.DataFrame(df)
+df.to_csv("D:\hepatitis\hepatitis_most_num.csv")
 
-d7f = pd.DataFrame(df)
-df.to_csv("D:\hepatitis\hepatitis_most.txt")
